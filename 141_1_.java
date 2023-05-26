08c9903ed7a106cf8a16eaac17a622b7d7583842
Ismielabir <ismielabir286@gmail.com>
2023-05-26 10:55:10 +0600

    LinkedList Reverse

---
+++ 141_1_.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+       ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+
+            if(slow == fast)
+            return true;
+            slow = slow.next;
+        }
+        return false;
+    }
+}
